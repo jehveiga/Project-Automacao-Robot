c2e3644265742addd25830aa373979bf7bc9cee6
Jefferson Veiga <gelsvsampa@hotmail.com>
2022-12-24 20:11:58 -0300

    Adicionar Task para ter um  timeOut entre eventos

--- Project_Automacao_Robot/Program.cs
+++ Project_Automacao_Robot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium;
+using System.Diagnostics;
 
 //Definir o driver do Browser a ser utilizado na aplicação atual criando uma nova instancia de Driver
 var driver = new ChromeDriver();
@@ -8,8 +9,14 @@ var driver = new ChromeDriver();
 //Adiciona uma URL para ser acessada no Web Driver e navega até ela pelo método
 driver.Navigate().GoToUrl("https://google.com.br");
 
+await Task.Delay(3000);
+
 //Find para encontrar o elemento ná pagina atual navegada e SendKeys para executar ações no elemento encontrado
 driver.FindElement(By.Name("q")).SendKeys("Como Aprender a programar?" + Keys.Enter);
 
+//Cria uma task para gerar  um timeOut
+await Task.Delay(5000);
 //Fechando a janela
-//driver.Quit();
+driver.Quit();
+
+//Environment.Exit(0);
