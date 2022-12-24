2bf1bb8b5d9853fc69c9f02fe4899a66636a0b3c
Jefferson Veiga <gelsvsampa@hotmail.com>
2022-12-24 18:21:34 -0300

    Efetuar uma pesquisa no google automatizada

--- Project_Automacao_Robot/Program.cs
+++ Project_Automacao_Robot/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+//Definir o driver do Browser a ser utilizado na aplicação atual criando uma nova instancia de Driver
+var driver = new ChromeDriver();
+
+//Adiciona uma URL para ser acessada no Web Driver e navega até ela pelo método
+driver.Navigate().GoToUrl("https://google.com.br");
+
+//Find para encontrar o elemento ná pagina atual navegada e SendKeys para executar ações no elemento encontrado
+driver.FindElement(By.Name("q")).SendKeys("Como Aprender a programar?" + Keys.Enter);
+
+//Fechando a janela
+//driver.Quit();
